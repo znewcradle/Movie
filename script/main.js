a9b1ae5b22e522d7cb5e252197900bfc2a6307c6
znewcradle <yunjiaxu_sse@qq.com>
2017-06-04 20:56:35 +0800

    This is the initial commit

--- script/main.js
+++ script/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by Xu on 2017/6/4.
  */
+$(function () {
+    /*************** 为search bar添加动态效果  **********/
+    $("#inpt_search").on('focus', function () {
+        $(this).parent('label').addClass('active');
+    }).on('blur', function () {
+        if ($(this).val().length == 0)
+            $(this).parent('label').removeClass('active');
+    }).on('mouseover', function(){
+         $("div.cntr-innr p").css("display", "block")
+             .removeClass("hover");
+    }).on('mouseout', function(){
+         $("div.cntr-innr p").css("display", "inline-block")
+             .addClass("hover");
+    });
+
+});
