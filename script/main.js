51c0861c9169a765f90dd82a080b8f691ab8994d
znewcradle <yunjiaxu_sse@qq.com>
2017-06-06 17:59:01 +0800

    This is the initial commit

--- script/main.js
+++ script/main.js
@@ -18,5 +18,9 @@ $(function () {
     /************** 增加动态效果********************************/
     $("div.main div.index-content").addClass("animated fadeInDown");
     $("div.main div.search-bar").addClass("animated fadeInUp");
-    
+    $("label.search").mouseover(function(){
+        $("div.main").addClass("fading");
+    }).mouseleave(function(){
+        $("div.main").removeClass("fading");
+    });
 });
